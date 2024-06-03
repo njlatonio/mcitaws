25070ded753738024f96d2dba7c18d2473b68e7b
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-03 18:52:12 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version=">=1.6.6" #this version is for Terraform Version, not aws
+    cloud {
+          organization = "MCIT-Nichelle"
+        workspaces {
+          name = "mcitaws"
+      }
+    }
+}
+
+provider "aws" {
+  region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_access_key
+}

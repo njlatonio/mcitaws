f3c57e7b9714f07316a122dab713fb18bcf0a8c5
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-03 19:33:57 -0400

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -5,8 +5,12 @@ resource "aws_s3_bucket" "bucket1" {
         }
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+}
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.bucket1.id
+  bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Disabled"
   }

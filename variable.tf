d23a69d8a634e8d910767803259680449f6b346b
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-03 18:53:32 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key"{
+}
+variable "aws_secret_access_key"{
+}
+
+variable "aws_region" {
+    description = "Region in which AWS Resources to be created"
+    type = string
+    default ="us-east-1"
+}
+variable "instance_type"{
+    description="EC2 Instance type"
+    type =string
+    default="t3.micro"
+}

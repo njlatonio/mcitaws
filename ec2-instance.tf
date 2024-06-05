5579d3744b3946ac982b0bc1547be3fbefcdb6cd
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-05 19:24:14 -0400

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -20,7 +20,7 @@ locals{
 }
 
 resource "aws_instance""ec2instance_example"{
-    for_each = { for instance in local.ec2instance_list: "${instance.name}" => instance }
+    for_each = { for instance in local.ec2instance_list: "${instance.tagname}" => instance }
     ami=data.aws_ami.amz_linux2.id
     instance_type = each.value.instancetype
     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]

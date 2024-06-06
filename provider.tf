d782a458478ae1d518aa32f6db38ba2cf2dcb452
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-05 20:00:47 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version=">=1.6.6" #this version is for Terraform Version, not aws
-    cloud {
-          organization = "MCIT-Nichelle"
-        workspaces {
-          name = "mcitaws"
-      }
-    }
-}
-
 provider "aws" {
   region = var.aws_region
   access_key = var.aws_access_key
